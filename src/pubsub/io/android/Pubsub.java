cb29b9c7c061709a8ccba7dd8190b728c7a66dbb
Andreas Göransson <ksango@MAH26019.(none)>
2011-11-18 11:41:17 +0100

    Added license information. (GPLv3)

--- src/pubsub/io/android/Pubsub.java
+++ src/pubsub/io/android/Pubsub.java
@@ -1,3 +1,21 @@
+/*
+ * pubsub.io Android Library
+ * Copyright (C) 2011  Andreas Göransson
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pubsub.io.android;
 
 import java.io.IOException;
@@ -10,12 +28,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Pubsub.io Android service class. This class allows an Android app to send to,
@@ -144,6 +166,14 @@ public class Pubsub extends Service {
 	public void connect(String host, String port, String sub) {
 		if (DEBUG)
 			Log.i(TAG, "connect(" + host + ", " + port + ", " + sub + ")");
+
+		if (!hasInternet()) {
+			Toast.makeText(
+					getApplicationContext(),
+					"Your device needs internet connection! Connection aborted!",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
 
 		mHost = host;
 		mPort = port;
@@ -346,6 +376,36 @@ public class Pubsub extends Service {
 			Log.i(TAG, "Write: " + message);
 
 		mPubsubComm.write(message.getBytes());
+	}
+
+	public boolean hasInternet() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		if (DEBUG)
+			Log.i(TAG, "Testing WiFi status");
+
+		// First test wifi for status!
+		NetworkInfo netInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			if (DEBUG)
+				Log.i(TAG, "WiFi detected, connecting");
+			return true;
+		}
+
+		if (DEBUG)
+			Log.i(TAG, "No WiFi detected, trying mobile");
+
+		netInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			if (DEBUG)
+				Log.i(TAG, "Mobile detected, connecting");
+			return true;
+		}
+
+		if (DEBUG)
+			Log.i(TAG, "No Mobile detected, aborting");
+
+		return false;
 	}
 
 	/**
